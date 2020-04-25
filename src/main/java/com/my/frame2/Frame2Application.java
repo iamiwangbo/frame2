bd9d8d1e5f4f0c628351e13d34da402808ba67a6
iamiwangbo <iamiwangbo@126.com>
2020-04-26 00:06:42 +0800

    java文件

---
+++ src/main/java/com/my/frame2/Frame2Application.java
@@ -0,0 +1,13 @@
+package com.my.frame2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Frame2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Frame2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/frame2/Frame2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.frame2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Frame2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
