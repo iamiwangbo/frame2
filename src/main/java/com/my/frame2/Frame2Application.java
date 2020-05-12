042748f7fd6e62fefbe3581ee617cc80d1c7ff4f
wangbo <wangbo@worktrans.cn>
2020-05-12 19:59:36 +0800

    配置mybaitis扫包

--- src/main/java/com/my/frame2/Frame2Application.java
+++ src/main/java/com/my/frame2/Frame2Application.java
@@ -3,11 +3,14 @@ package com.my.frame2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wangbo
+ */
 @SpringBootApplication
 public class Frame2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Frame2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Frame2Application.class, args);
+    }
 
 }
---
+++ src/main/java/com/my/frame2/myframe/common/mybatis/MapperScannerConfig.java
@@ -0,0 +1,34 @@
+package com.my.frame2.myframe.common.mybatis;
+
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tk.mybatis.spring.mapper.MapperScannerConfigurer;
+
+import java.util.Properties;
+
+/**
+ * @author 王波
+ * @date 2020/5/9
+ */
+@Configuration
+@AutoConfigureAfter(MybatisAutoConfiguration.class)
+public class MapperScannerConfig {
+
+    @Bean
+    public static MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+        // mapper类的包名--即操作数据库的对应到数据表的Mapper.java类的包名，如果这里的包名配错项目启动报错
+        mapperScannerConfigurer.setBasePackage("com.test.mapper.customer");
+
+        // 这里properties的相关配置，由于能力有限，暂时还未探究，如果你知道请麻烦告诉我，谢谢
+        Properties properties = new Properties();
+        properties.setProperty("notEmpty", "false");
+        properties.setProperty("IDENTITY", "MYSQL");
+        properties.setProperty("ORDER", "BEFORE");
+        mapperScannerConfigurer.setProperties(properties);
+        return mapperScannerConfigurer;
+    }
+}
